be3384e8cfc93ef55ac68f2e7d8e7f81f82d0c6b
Saicharan <nagillasaicharan007@gmail.com>
2026-05-30 11:18:49 +0530

    Use screenshot logo asset in app

--- deepfocus-site/src/components/DeepFocusLogo.jsx
+++ deepfocus-site/src/components/DeepFocusLogo.jsx
@@ -12,9 +12,9 @@ export default function DeepFocusLogo({
         className={`relative flex h-8 w-11 shrink-0 items-center justify-center overflow-hidden rounded-lg border border-white/[0.08] bg-black shadow-[0_8px_24px_rgba(0,0,0,0.45)] ${markClassName}`}
       >
         <img
-          src="/deepfocus-logo.png"
+          src="/screenshots/deepfocusLogo.png"
           alt="DeepFocus"
-          className="h-full w-full object-contain scale-[1.08]"
+          className="h-full w-full object-cover object-[50%_62%] scale-[1.9]"
           draggable="false"
         />
       </span>
